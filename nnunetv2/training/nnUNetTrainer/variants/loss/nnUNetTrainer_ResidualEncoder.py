569e76d3a2273788631e68697a7feacbba270863
sgtm-sougata <sgtm.tmc@gmail.com>
2026-01-27 13:10:59 +0530

    updated num input channels

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_ResidualEncoder.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_ResidualEncoder.py
@@ -6,10 +6,10 @@ from dynamic_network_architectures.architectures.unet import ResidualEncoderUNet
 
 class nnUNetTrainer_ResidualEncoder(nnUNetTrainer):
     def build_network_architecture(self, plans_manager: PlansManager,
-                                   dataset_json,
-                                   configuration_manager: ConfigurationManager,
-                                   num_input_channels,
-                                   enable_deep_supervision: bool = True) -> nn.Module:
+                               dataset_json,
+                               configuration_manager: ConfigurationManager,
+                               num_input_channels=1,  # <--- add default here
+                               enable_deep_supervision: bool = True) -> nn.Module:
         
         # Override the default PlainConvUNet with ResidualEncoderUNet
         model = ResidualEncoderUNet(
